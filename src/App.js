c5cb7bd8f520ddd14a935fb4c314a691e0b80fc4
abhijeet-qsofte <abhijeet@qsofte>
2025-05-06 19:54:45 +1200

    Fix component imports

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
+import Header from './Components/Header';
 import Footer from './Components/Footer';
 import Home from './pages/Home';
 import OrderNow from './pages/OrderNow';
@@ -11,7 +11,7 @@ function App() {
   return (
     <Router>
       <div className="flex flex-col min-h-screen">
-        <Navbar />
+        <Header />
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<Home />} />
