55e6ac1991a61de56c3471b2fb6837294342e057
Mayank-Qsofte <mayank@qsofte.com>
2025-05-20 17:30:16 +0530

    Update Privacy.js

--- src/pages/Privacy.js
+++ src/pages/Privacy.js
@@ -13,7 +13,7 @@ const Privacy = () => {
       >
         <div className="absolute inset-0 bg-black bg-opacity-60"></div>
         <h1 className="relative z-10 text-4xl md:text-5xl font-gilroy-semibold text-white">
-          Privacy Policy
+          Our policies
         </h1>
       </div>
 
