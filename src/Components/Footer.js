a5f9bc90194f895ffeb55f82b2a3fcb0a44751dd
pratikrakash <pratikrakash@gmail.com>
2026-03-02 20:17:58 +1100

    Fixed company name representation on the footer of the web page.

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -21,7 +21,10 @@ const Footer = () => {
           {/* About Section */}
           <div className="md:w-1/2 lg:w-5/12">
             <img src={logo} alt={t('footer.logoAlt')} className="h-6 mb-0" />
-            <h2 className="font-gilroy-medium text-lg mb-1 whitespace-nowrap">
+            {/* <h2 className="font-gilroy-medium text-lg mb-1 whitespace-nowrap">
+              {t('footer.name')}
+            </h2> */}
+            <h2 className="font-gilroy-medium text-lg mb-1 whitespace-pre-line leading-tight">
               {t('footer.name')}
             </h2>
             {/* Social Media Icons */}
